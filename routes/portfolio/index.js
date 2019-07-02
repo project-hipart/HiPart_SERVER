c36905702111df6496a3623bf69066953cf07de4
ingue <dlsrb8949@gmail.com>
2019-07-02 17:42:30 +0900

    portfolio router commit#1

---
+++ routes/portfolio/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+// router.use("/creator", require("./creator"));
+// router.use("/editor", require("./editor"));
+// router.use("/translator", require("./translator"));
+// router.use("/etc", require("./etc"));
+
+
+module.exports = router;
