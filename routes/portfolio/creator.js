87e3399b84a07a1e7a5bb0bf458b3382a53cb6d8
ingue <dlsrb8949@gmail.com>
2019-07-02 19:50:53 +0900

    creator commit#1

--- routes/portfolio/creator.js
+++ routes/portfolio/creator.js
@@ -3,13 +3,27 @@ var router = express.Router();
 
 const upload = require('../../config/multer');
 const crypto = require('crypto-promise');
+var moment = require('moment');
+
+const authUtil = require("../../module/utils/authUtils");
+
 
 const defaultRes = require('../../module/utils/utils');
 const statusCode = require('../../module/utils/statusCode');
 const resMessage = require('../../module/utils/responseMessage')
 const db = require('../../module/pool');
 
-router.post('/', async (req, res) => {
+router.post('/', upload.single('thumbnail'), authUtil.isLoggedin, async (req, res) => {
+
+    const insertQuery = 'INSERT INTO creator (thumbnail, url, title, content, user_idx, createdAt) VALUES (?, ?, ?, ?, ?, ?)';
+    const insertResult = await db.queryParam_Arr(insertQuery,
+        [req.file.location, req.body.url, req.body.title, req.body.content, req.decoded.idx, moment().format('YYYY-MM-DD HH:mm:ss')]);
+    if (!insertResult) {
+        res.status(200).send(defaultRes.successFalse(statusCode.DB_ERROR, resMessage.FAILED_INSERT_WORK));
+    } else {
+        res.status(200).send(defaultRes.successTrue(statusCode.OK, resMessage.SUCCESS_INSERT_WORK));
+    }
+
 
 });
 
