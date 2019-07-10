a1afbf9a6f8ac88366a87ec01ed597e2b6881e63
ingue <dlsrb8949@gmail.com>
2019-07-10 23:04:05 +0900

    commit mypage2 #1

--- routes/mypage/index.js
+++ routes/mypage/index.js
@@ -4,5 +4,6 @@ var router = express.Router();
 router.use("/question", require("./question"));
 router.use("/modify", require("./modify"));
 router.use("/info", require("./info"));
-
+router.use("/pick", require("./pick"));
+router.use("/hifive", require("./hifive"));
 module.exports = router;
