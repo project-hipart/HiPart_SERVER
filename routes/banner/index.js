f1d2655e2485182b4424f65b52d7d331bbce2bf9
양희연 <hhyyeon0214@gmail.com>
2019-07-10 23:11:44 +0900

    commit banner/index #1

--- routes/banner/index.js
+++ routes/banner/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 router.use("/list", require("./list"));
 router.use("/click", require("./click"));
-
-
+router.use("/detail", require("./detail"));
+router.use("/apply", require("./apply"));
+router.use("/simple", require("./simple"));
 
 module.exports = router;
