e79b23ce9cccc272566aa35a2324cc03017ec45f
양희연 <hhyyeon0214@gmail.com>
2019-07-13 06:32:30 +0900

    commit auth/index #3

--- routes/auth/index.js
+++ routes/auth/index.js
@@ -6,5 +6,6 @@ router.use("/signin", require("./signin"));
 router.use("/signup", require("./signup"));
 router.use("/refresh", require("./refresh"));
 router.use("/duplicated", require("./duplicated"));
+router.use("/finder", require("./finder"));
 
 module.exports = router;
